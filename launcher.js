d800829dea20349aa1b1fbff9802e7c6f53f9eb4
Kris Neal <kisflying@gmail.com>
2015-08-04 21:36:57 +0800

    ..

--- launcher.js
+++ launcher.js
@@ -1,11 +1,37 @@
+/*
+Get the JS API ticket, it's temporary access method for single request.
+The ticket should be stored at server side.
+*/
+function getApiTicketTemporary(){
+	$.get( {
+		url: "https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=wxdebf3e2511cf03f7&secret=73cea8e8e906b72c86ce00ba47ab625a",
+		crossDomain: true, 
+		dataType: 'jsonp',
+		success: function( data ) {
+			console.log(data);
+
+			var token = data.access_token;
+
+			$.get({
+				url: "https://api.weixin.qq.com/cgi-bin/ticket/getticket?type=jsapi&access_token=" + token,
+				crossDomain: true, 
+				dataType: 'jsonp',
+				success: function( data ) {
+					console.log(data);
+
+					alert(data.ticket);
+				},
+			});
+		}
+	});
+}
+
 
 /*
 Fire every button.
 */
 $( "#button_init" ).click( function( event ) {
-	//wxapi_init();
-
-	getApiTicketTemporary();
+	wxapi_init();
 });
 
 $( "#button_shareFriend" ).click( function( event ) {
--- wechat-api-lib.js
+++ wechat-api-lib.js
@@ -18,34 +18,6 @@ function makeSignature(jsapiTicket, randomKey, timeStamp, url){
 }
 
 /*
-Get the JS API ticket, it's temporary access method for single request.
-The ticket should be stored at server side.
-*/
-function getApiTicketTemporary(){
-	$.get( {
-		url: "https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=wxdebf3e2511cf03f7&secret=73cea8e8e906b72c86ce00ba47ab625a",
-		crossDomain: true, 
-		dataType: 'jsonp',
-		success: function( data ) {
-			console.log(data);
-
-			var token = data.access_token;
-
-			$.get({
-				url: "https://api.weixin.qq.com/cgi-bin/ticket/getticket?type=jsapi&access_token=" + token,
-				crossDomain: true, 
-				dataType: 'jsonp',
-				success: function( data ) {
-					console.log(data);
-
-					alert(data.ticket);
-				},
-			});
-		}
-	});
-}
-
-/*
 Init all configurations for current page request.
 */
 function wxapi_init(ticket, appId){
